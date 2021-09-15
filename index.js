a993c9067f2197ceb458d1d165e757d5f0b8d63b
Tim Tattersall <tim@tattsy.com>
2021-09-15 20:31:57 +1000

    Allowing wifi to be tested on local machines

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ const configLoader = require('./config.js');
 
 controlPanel.init(app);
 cloudDeakin.initMock(app);
-
-hardware.scanWifi(); // Scans network for all aval wifis and saves to array
-wifi = hardware.getWifi(); // Array which can then be printed or displayed in any way (ONLY SSIDS ARE SAVED)
-
+hardware.init(app);
 
 app.listen(9000, () => {
     console.log('running server on port 9000')
